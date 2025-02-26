ebb060d2b9d1eef791c3c220ecdcf37d320e8f52
oorgrim <oorgrim@gmail.com>
2025-02-26 18:51:56 +0500

    logic and function of saving result are added

---
+++ ascii console/ascii console/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace ascii_console
+{
+    internal class Program
+    {
+        private const double WIDTH_OFFSET = 2.5;
+        private const int MAX_WIDTH = 400;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *JPEG"
+            };
+            //openFileDialog.ShowDialog();
+            Console.WriteLine("Press enter to start...\n");
+
+            while (true)
+            {
+                Console.ReadLine();
+                if (openFileDialog.ShowDialog() != DialogResult.OK) 
+                    continue;
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscale();
+                //char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+
+                foreach (var row in rows)
+                    Console.WriteLine(row);
+
+                File.WriteAllLines("image.txt", rows.Select(r => new string(r)));
+                Console.SetCursorPosition(0, 0);
+            }
+
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var maxWidth = 350;
+            var newHeight = bitmap.Height / WIDTH_OFFSET * maxWidth / bitmap.Width;
+            if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+            return bitmap;
+        }
+    }
+}
---
+++ ascii console/ascii console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ascii console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ascii console")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c7d0c7d5-825a-49d2-b00b-8db5a29ab181")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
